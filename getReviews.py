15c6f308b6da3fd1f8e367d793e433db3a9b3f42
mike lorengo <mikelor@gmail.com>
2020-10-12 15:27:33 -0700

    Extracts ReviewId, Rating and Itinerary

--- getReviews.py
+++ getReviews.py
@@ -1,7 +1,35 @@
-import time
+from bs4 import BeautifulSoup
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+import time
+
+#
+# Classes
+#
+class Review:
+    def __init__(self, id, rating):
+        self.Id = id
+        self.Rating = rating
+        self.Itinerary = ""
+
+        self.Region = ""
+        self.Class = ""
+        self.DateOfReview = ""
+        self.DateofTravel = ""
+        self.Title = ""
+        self.Text = ""
+
+    def setItinerary(self, itinerary):
+        self.Itinerary = itinerary
+
+
+class Itinerary:
+    def __init__(self, origin, destination):
+        self.Origin = origin
+        self.Destination = destination
+    
 
 # startWebDriverService()
 # Starts the web driver as a service, this improves performance and does not require a manual start
@@ -21,6 +49,24 @@ def startWebDriver():
     driver = webdriver.Chrome('./thirdparty/chromedriver.exe')
     return driver
 
+
+
+def getReviews(driver, reviewIds):
+    return
+
+# parseReview
+def parseReviewDiv(reviewDiv):
+    review = ""
+    print(reviewDiv.text)
+    
+    return review
+
+
+
+
+#
+# getReviewCount()
+#
 # Get's the total number of reviews for a property
 #   driver: A handle to the selenium webdriver
 #   class:  The specific class of the span tag that holds the review count
@@ -33,63 +79,89 @@ def getReviewCount(driver, classId):
 
     return reviewCount
 
-def getReviewIds(driver):
-    return
+#
+# getAllReviews(driver, url, reviewCount)
+#
+def getReviews(driver, url, reviewCount):
 
-def getReviews(driver, reviewIds):
-    return
+    urlTemplate = url.replace('.html', '-or{}.html')
 
-# parseReviews
-def parseReviews(driver, url):
+    reviews = []
+    offset = 0
+
+    while(True):
+        reviewUrl = urlTemplate.format(offset)
+
+        reviewSubset = getReviewsForUrl(driver, reviewUrl)
+        if not reviewSubset:
+            break
+
+        reviews += reviewSubset
+
+        if len(reviewSubset) < 5:
+            break
+      
+        offset += 5
+
+        # For testing purposes, let's stop after fetching a few reviews
+        if offset > 5:
+            break
+
+    return reviews
+
+#
+# getReviewsForUrl
+#
+def getReviewsForUrl(driver, url):
     # Get the review url page
     driver.get(url)
     time.sleep(5)
 
     companyName = driver.find_element_by_xpath('//h1[@class="_3ggwzaPV"]')
+    
+    reviews = []
+    reviewDivs = driver.find_elements_by_xpath('//div[@data-reviewid]')
+    for reviewDiv in reviewDivs:
+        reviewId = reviewDiv.get_attribute('data-reviewid') 
+        rating   = getReviewRating(reviewDiv)
+        review   = Review(reviewId, rating)
 
-    reviewIds = getReviewIds(driver)
-    if not reviewIds:
-        return
+        review.setItinerary(getReviewItinerary(reviewDiv))
 
-    getReviews(driver, reviewIds)
+        # Add the review to the list
+        reviews.append(review)
 
-    items = []
-    print()
+    return reviews
 
-    return reviewIds
+#
+# getReviewRating(reviewDiv)
+#
+# The rating for the review is stored as a class on an inner span with two classes applied
+# The first class identifies it as a "bubble rating", while the second defines the level from 1 to 5
+# 
+# Example class="ui_bubble_rating bubble_10"
+#
+def getReviewRating(reviewDiv):
+    ratingString = (((reviewDiv.find_element_by_class_name('ui_bubble_rating')).get_attribute('class')).split())[1]
+    rating = (int(ratingString[-2:]))
+    rating = rating/10
 
-# getAllReviews
-def getAllReviews(driver, url, reviewCount):
+    return rating
 
-    urlTemplate = url.replace('.html', '-or{}.html')
+#
+# getReviewItinerary
+#
+def getReviewItinerary(reviewDiv):
+    itineraryString = (reviewDiv.find_elements_by_class_name('_3tp-5a1G')[0]).text
+    itineraryList = itineraryString.split(' - ')
+    itinerary = Itinerary(itineraryList[0], itineraryList[1])
 
-    items = []
-    offset = 0
-
-    while(True):
-        subpageUrl = urlTemplate.format(offset)
-
-        subpageItems = parseReviews(driver, subpageUrl)
-        if not subpageItems:
-            break
-
-        items += subpageItems
-
-        if len(subpageItems) < 5:
-            break
-
-      
-        offset += 5
-
-        # For testing purposes, let's stop after fetching a few reviews
-        if offset > 10:
-            break
-
-    return items
-
-
-
+    return itinerary
+####
+#
 # Entry Point
+#
+####
 #driver = startWebDriverService()
 driver = startWebDriver()
 
@@ -98,9 +170,10 @@ baseUrl = 'https://www.tripadvisor.com/Airline_Review-d8729017-Reviews-Alaska-Ai
 driver.get(baseUrl)
 time.sleep(5) 
 
-
 airlineReviewCountClassId = '_2tNtmCyi'
 reviewCount = getReviewCount(driver, airlineReviewCountClassId)
-print(reviewCount)
 
-getAllReviews(driver, baseUrl, reviewCount)
+reviews = getReviews(driver, baseUrl, reviewCount)
+
+for review in reviews:
+    print(review.Id, review.Rating, review.Itinerary.Origin, review.Itinerary.Destination)
